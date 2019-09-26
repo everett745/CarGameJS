969fe2f5af16712d69241b4145b8a52194f5bae4
everett745 <kirillavdonin2014@yandex.ru>
2019-09-26 20:25:32 +0300

    asd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const score = document.querySelector('.score'),
+const score = document.querySelector('.score span'),
+    topScore = document.querySelector('.localStorage span'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
@@ -20,6 +21,12 @@ const setting = {
   traffic: 3  
 };
 
+const cars = {
+    1: 'url(./image/enemy.png)',
+    2: 'url(./image/enemy2.png)',
+    3: 'url(./image/player.png)'
+};
+
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
 document.addEventListener('keyup', stopRun);
@@ -29,7 +36,19 @@ function getQuantityElements(heightElement) {
 }
 
 function startGame() {
+    if(event.target.tagName == "DIV") {return 0;}
+
+    switch (event.target.dataset.diff) {
+        case 'easy': setting.speed = 3; setting.traffic = 4; break;
+        case 'middle': setting.speed = 6; setting.traffic = 3; break;
+        case 'hard': setting.speed = 9; setting.traffic = 2; break;
+    }
+
+    startMusic();
+
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+    car.style.cssText = 'left: 125px; bottom: 10px;';
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
@@ -45,9 +64,12 @@ function startGame() {
         enemy.y = -100 * setting.traffic * (i + 1);
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         enemy.style.top = enemy.y + 'px';
+        let rndCar = Math.round(Math.random() * 4) - 1;
+        enemy.style.background = "transparent " + cars[rndCar] + " center / cover no-repeat";
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
 
@@ -58,9 +80,11 @@ function startGame() {
 }
 
 function playGame() {
-    moveRoad();
-    moveEnemy();
     if (setting.start) {
+        setting.score += setting.speed;
+        score.textContent = setting.score;
+        moveRoad();
+        moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
             setting.x -= setting.speed;
         }
@@ -105,12 +129,52 @@ function moveRoad() {
 
 function moveEnemy() {
     const enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(element => {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = element.getBoundingClientRect();
+
+        if(carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left && 
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top){
+            setting.start = false;
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight * 2 + 'px';
+            printTopResult();
+            stopMusic();
+        }
+
         element.y += setting.speed / 1.5;
         element.style.top = element.y;
         if(element.y >= document.documentElement.clientHeight){
-            element.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+            let rndCar = Math.floor(Math.random() * 4) - 1;
+            element.style.background = "transparent " + cars[rndCar] + " center / cover no-repeat";
+            element.style.left = Math.round(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
             element.y = -100 * setting.traffic;
         }
     });
+}
+
+function printTopResult() {
+    if(parseInt(topScore.textContent) < parseInt(score.textContent)){
+        topScore.textContent = score.textContent;
+        score.innerHTML = "Новый рекорд: " + score.textContent;
+    }
+}
+
+var startAudio = new Audio();
+var stopAudio = new Audio();
+function startMusic() {
+    stopAudio.pause();
+
+    startAudio.src = './sound/Start.mp3'; // Указываем путь к звуку "клика"
+    startAudio.autoplay = true; // Автоматически запускаем
+}
+
+function stopMusic() {
+    stopAudio.src = './sound/Stop.mp3';
+    stopAudio.autoplay = true;
+
+    startAudio.pause();
 }
